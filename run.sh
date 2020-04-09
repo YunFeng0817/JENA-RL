e68bbf2c612f475664a1ac02af9d0c4d7ad4db10
Mark <f18846188605@gmail.com>
2020-04-09 21:03:21 +0800

    Added tensorflow module

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 mvn -Drat.skip=true compile
 # mvn exec:java -Dexec.mainClass="StarterSDB"
-mvn exec:java -Dexec.mainClass="StarterTDB"
+# mvn -q exec:java -Dexec.mainClass="StarterTDB"
+mvn -q exec:java -Dexec.mainClass="TensorFlowDemo"
 # mvn exec:java -Dexec.mainClass="com.vineetmanohar.module.Main" -Dexec.args="arg0 arg1 arg2"
 # cd /home/mark/program/GraduateProject/jena-sdb ; /usr/lib/jvm/java-8-openjdk-amd64/bin/java -Dfile.encoding=UTF-8 -cp /tmp/cp_3vz5ep8ur8spmthcvw6fkifxm.jar Starter
